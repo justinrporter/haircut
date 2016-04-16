7c09ad9dcbdb527573a591f850248a41edcafeff
Justin R. Porter <justinrporter@gmail.com>
2016-04-16 18:10:58 -0500

    build basic contestant model.

--- app/models.py
+++ app/models.py
@@ -1,5 +1,19 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+from django.conf import settings
 
-# Create your models here.
+class Contestant(models.Model):
+    #the user model should have email, name, etc.
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+
+    bio = models.TextField()
+    pic = models.ImageField()
+
+    # maybe not necessary, but you'll be able to call contestant.first_name
+    # intstead of contestant.user.first_name
+    def first_name(self):
+        return user.first_name
+    def last_name(self):
+        return user.last_name
