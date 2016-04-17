95fe84e3835dbba9f777d20921fe0ca19d79d7a4
Justin R. Porter <justinrporter@gmail.com>
2016-04-17 18:14:47 -0500

    Build out views for haircut and contestant.

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$',
-        views.home,
-        name="home"),
+    url(r'^$', views.home, name="home"),
+    url(r'^contestant/(?P<pk>[-\w]+)/$',
+        views.ContestantDetailView.as_view(),
+        name='contestant-detail'),
+    url(r'^haircut/(?P<pk>[-\w]+)/$',
+        views.HaircutDetailView.as_view(),
+        name='haircut-detail'),
 ]
