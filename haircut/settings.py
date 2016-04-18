a41c234d4dbb01f66b83e8603c8e526c1064d530
rizzomichaelg <rizzo.michaelg@gmail.com>
2016-04-18 18:30:30 -0500

    debug back on

--- haircut/settings.py
+++ haircut/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=-n20ckxfssoc7kti@-j=k4ike#j3ap8j6t@#b6x_d9liy!-x1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
